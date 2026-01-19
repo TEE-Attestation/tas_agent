af6aece92cd47cd1fec32fba9ae3a1be3e72079f
Daniel P. Berrangé <berrange@redhat.com>
2026-01-19 17:25:51 +0000

    Use the standard 'log' crate for debug messages
    The main user visible change at this point is that the log output now
    also includes timestamps, allowing correlation between the TAS agent
    and TAS server logs.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- src/tee_evidence.rs
+++ src/tee_evidence.rs
@@ -8,39 +8,31 @@
 // TEE Evidence gathering functionality.
 //
 use base64::{engine::general_purpose, Engine};
+use log::debug;
 use std::error::Error;
 use std::fs;
 use tempfile::{tempdir_in, TempDir};
 
 // TODO : implement own error handling, use boxed errors for now
 
-/// Prints debug messages to stdout if the debug flag (-d) is enabled.
-macro_rules! debug_println {
-    ($debug:expr, $($arg:tt)*) => {
-        if $debug {
-            println!($($arg)*);
-        }
-    };
-}
-
 // Internal function to determine the TEE type
 // This function returns the TEE type as a string (e.g., "amd-sev-snp").
-fn get_tee_type(debug: bool, tsm_report_dir: &TempDir) -> Result<String, Box<dyn Error>> {
+fn get_tee_type(tsm_report_dir: &TempDir) -> Result<String, Box<dyn Error>> {
     // determine TEE type dynamically using tsm report/provider
     let provider = fs::read_to_string(tsm_report_dir.path().join("provider"))?;
 
-    debug_println!(debug, "TSM provider: {}", provider.trim());
+    debug!("TSM provider: {}", provider.trim());
     match provider.trim() {
         "sev_guest" => {
-            debug_println!(debug, "Determined TEE type: amd-sev-snp");
+            debug!("Determined TEE type: amd-sev-snp");
             Ok("amd-sev-snp".to_string())
         }
         "tdx_guest" => {
-            debug_println!(debug, "Determined TEE type: intel-tdx");
+            debug!("Determined TEE type: intel-tdx");
             Ok("intel-tdx".to_string())
         }
         other => {
-            debug_println!(debug, "Unknown TEE provider: {}", other);
+            debug!("Unknown TEE provider: {}", other);
             Err(format!("Unknown TEE provider: {}", other).into())
         }
     }
@@ -63,11 +55,10 @@ fn get_vmpl() -> Result<String, Box<dyn Error>> {
 /// Requires config_tsm to be enabled in the kernel.
 /// # Arguments
 /// * `nonce` - A string slice that holds the nonce value (must be exactly 64 bytes long)
-/// * `debug` - A boolean flag to enable debug output
 /// # Returns
 /// * `Result<(String, String), String>` - On success, returns a tuple containing the
 ///   Base64-encoded TEE evidence and the TEE type. On failure, returns an error message.
-pub fn tee_get_evidence(nonce: &str, debug: bool) -> Result<(String, String), String> {
+pub fn tee_get_evidence(nonce: &str) -> Result<(String, String), String> {
     // Setup temp_dir_path to the config tsm report path
     let temp_dir_path = "/sys/kernel/config/tsm/report";
 
@@ -87,44 +78,41 @@ pub fn tee_get_evidence(nonce: &str, debug: bool) -> Result<(String, String), St
     // Attempt to create a temporary directory inside the specified path
     let tmp_dir = tempdir_in(temp_dir_path)
         .map_err(|err| format!("Failed to create temp directory: {}", err))?;
-    debug_println!(debug, "Temp dir created at: {:?}", tmp_dir.path());
-    debug_println!(debug, "Nonce_bytes (hex): {}", hex::encode(nonce_bytes));
+    debug!("Temp dir created at: {:?}", tmp_dir.path());
+    debug!("Nonce_bytes (hex): {}", hex::encode(nonce_bytes));
 
     // Determine TEE type
-    let tee_type = get_tee_type(debug, &tmp_dir)
-        .map_err(|err| format!("Failed to determine TEE type: {}", err))?;
+    let tee_type =
+        get_tee_type(&tmp_dir).map_err(|err| format!("Failed to determine TEE type: {}", err))?;
 
     // Write nonce to inblob file
     let inblob_file_path = tmp_dir.path().join("inblob");
     fs::write(&inblob_file_path, nonce)
         .map_err(|err| format!("Failed to write to inblob file: {}", err))?;
-    debug_println!(debug, "Wrote to inblob file at: {:?}", inblob_file_path);
+    debug!("Wrote to inblob file at: {:?}", inblob_file_path);
 
     // if SEV get VMPL level dynamically else skip this step
     if tee_type == "amd-sev-snp" {
-        debug_println!(debug, "TEE type is SEV-SNP, setting VMPL level");
+        debug!("TEE type is SEV-SNP, setting VMPL level");
         // Set VMPL level
         let vmpl = get_vmpl().map_err(|err| format!("Failed to get VMPL: {}", err))?;
         let privlevel_path = tmp_dir.path().join("privlevel");
         fs::write(privlevel_path, &vmpl).map_err(|err| format!("Failed to set VMPL: {}", err))?;
-        debug_println!(debug, "Set VMPL level to: {}", vmpl);
+        debug!("Set VMPL level to: {}", vmpl);
     } else {
-        debug_println!(
-            debug,
-            "TEE type is not SEV-SNP, skipping VMPL level setting"
-        );
+        debug!("TEE type is not SEV-SNP, skipping VMPL level setting");
     }
 
     // Read outblob file
     let outblob_file_path = tmp_dir.path().join("outblob");
-    debug_println!(debug, "Reading outblob file at: {:?}", outblob_file_path);
+    debug!("Reading outblob file at: {:?}", outblob_file_path);
 
     let tee_report = fs::read(&outblob_file_path)
         .map_err(|err| format!("Failed to read outblob file: {}", err))?;
 
     // Drop the temporary directory
     drop(tmp_dir);
-    debug_println!(debug, "Temp dir dropped");
+    debug!("Temp dir dropped");
 
     // Base64 encode the SNP report using Engine::encode
     let encoded_report = general_purpose::STANDARD.encode(&tee_report);
